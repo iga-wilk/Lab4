20a4a349b39ae98bd860e57671e12a459b2e38b9
iga-wilk <74589714+iga-wilk@users.noreply.github.com>
2020-11-18 14:03:19 +0100

    Update Laboratorium4.java

--- Laboratorium4.java
+++ Laboratorium4.java
@@ -1,5 +1,10 @@
-import java.sql.*;
-import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;;
 
 public class Laboratorium4 {
 
